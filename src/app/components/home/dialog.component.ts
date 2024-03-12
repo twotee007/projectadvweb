a06aecd415d55f7f4d418058c5716f6d07676557
akarapon007 <147177816+akarapon007@users.noreply.github.com>
2024-03-12 17:45:57 +0700

    up dialog/home

--- src/app/components/home/dialog.component.ts
+++ src/app/components/home/dialog.component.ts
@@ -2,16 +2,16 @@ import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
+  styleUrl: './home.component.scss',
   selector: 'app-your-dialog',
   template: `
-<div class="d-flex justify-content-center align-items-center">
+<div class="d-flex justify-content-center align-items-center" [@slideIn]="true">
   <div class="text-center" style="width: 100%;">
     <h1 mat-dialog-title>{{data.winnername}} Winner!!!</h1>
     <img [src]="data.winnerImageSrc" alt="Winner Image" style="width: 400px; display: block; margin: 0 auto; border-radius: 15px;">
     <h4>Winner Score: {{data.winnerScore }} + {{data.Winnerrat}} => {{data.winnerScore + data.Winnerrat}}</h4>
   </div>
 </div>
-
   `,
 })
 export class DialogComponent {
